227d1f3b7b39c5f747a2f3066c7b637dd05ff9a9
mhafizumer <mhafizumer>
2025-11-11 12:40:43 +0500

    Add Test Module

---
+++ app/code/Tilemountain/CICD/Controller/Index/Index.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace code\Tilemountain\CICD\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Class Index
+ * @package Tilemountain\CICD\Controller\Index\Index
+ */
+class Index extends Action
+{
+
+
+    /**
+     * Index resultPageFactory
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        return parent::__construct($context);
+    }
+
+    /**
+     * Function execute
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
